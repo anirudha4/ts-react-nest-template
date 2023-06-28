b97f07c19d9e9ffe39a819239ecdcf1e88911681
Anirudha Gandhare <anirudha@chisellabs.com>
2023-06-28 23:21:19 +0530

    cleanup: remove unused imports

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,14 +1,12 @@
-import { Route, Routes, redirect } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // utilities
 import { PATHS } from "@config/constants/paths";
 // components
 import { Auth } from "@pages/auth";
 import { Home, Layout } from "@pages/home";
 import { Dashboard, DashboardLayout } from "@pages/dashboard";
-import useAuth from "@hooks/useAuth";
 
 const App = () => {
-  const { user } = useAuth();
   return (
     <Routes>
       <Route path={PATHS.HOME} element={<Layout />}>
