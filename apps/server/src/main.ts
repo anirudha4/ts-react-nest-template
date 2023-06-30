bafda34a50453acbf2a4e2576712b9bd641aab46
Anirudha Gandhare <anirudha@chisellabs.com>
2023-06-30 23:48:15 +0530

    feat: push public folder

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.use(cookieParser())
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
